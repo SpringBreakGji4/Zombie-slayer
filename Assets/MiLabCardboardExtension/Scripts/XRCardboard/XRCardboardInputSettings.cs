ba7a9d7ad3ea155429bcf83cf19282719359a641
albertlch20 <albertlch20@gmail.com>
2023-03-31 00:52:34 -0500

    Add player and pause menu

---
+++ Assets/MiLabCardboardExtension/Scripts/XRCardboard/XRCardboardInputSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "XRCardboardInputSettings", menuName = "Google Cardboard/Cardboard Input Settings")]
+public class XRCardboardInputSettings : ScriptableObject
+{
+    public string ClickInput => clickInputName;
+    public bool ClickOnHover => clickOnHover;
+    public float GazeTime => gazeTimeInSeconds;
+
+
+    [SerializeField]
+    string clickInputName = "Submit";
+    [SerializeField]
+    bool clickOnHover = false;
+    [SerializeField, Range(.5f, 5)]
+    float gazeTimeInSeconds = 2f;
+}
