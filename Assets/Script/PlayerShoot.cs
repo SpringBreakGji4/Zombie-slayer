69904ed3f73511e6e57d4159c9002c95bf59e181
William Chang <86466510+WilliamTPE@users.noreply.github.com>
2023-04-07 13:36:58 -0500

    Add controller manipulation

--- Assets/Script/PlayerShoot.cs
+++ Assets/Script/PlayerShoot.cs
@@ -19,7 +19,7 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("OK"))
         {
             Shoot();
         }
