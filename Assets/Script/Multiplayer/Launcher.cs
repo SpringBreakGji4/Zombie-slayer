926384c52cb2cd4611bf049ddd3b1c9c981c077f
albertlch20 <albertlch20@gmail.com>
2023-04-11 20:28:14 -0500

    Add control instruction on starting menu, fix minor bugs

--- Assets/Script/Multiplayer/Launcher.cs
+++ Assets/Script/Multiplayer/Launcher.cs
@@ -125,4 +125,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
